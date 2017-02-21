d0791a9f445ec63792b0dde9b80475dd2babd537
Gricel Dayhanna Sepúveda Rozas <gr3c2l@gmail.com>
2017-02-21 10:28:23 -0300

    progress-pops-navs-ok_v1

--- js/scripts.js
+++ js/scripts.js
@@ -14,54 +14,155 @@ $(document).ready(function(){
     });
   // TRIGGER TOOL MENU
   $(".cg-main_li_tool").click(function(){
-    if($(".cg-menu_tool").hasClass("display"))
+    if($(".cg-menu_tool").hasClass("cg-display"))
         {
-          $(".cg-menu_tool").removeClass("display")
+          $(".cg-menu_tool").removeClass("cg-display");
         }
     else{
-          $(".cg-menu_tool").addClass("display")
+          $(".cg-menu_tool").addClass("cg-display");
         }
   });
   $(".cg-menu_tool").mouseleave(function(){
-      $(".cg-menu_tool").removeClass("display")
+      $(".cg-menu_tool").removeClass("cg-display");
   });
   $(".cg-tool_li").click(function(){
-    $(".cg-menu_tool").removeClass("display");
+    $(".cg-menu_tool").removeClass("cg-display");
     $(".cg-menu_ul").animate
       ({
         "left":'-100vw'
       }, 350);
   });
   $(".cg-main_li_out").hover(function(){
-      $(".cg-menu_tool").removeClass("display");
+      $(".cg-menu_tool").removeClass("cg-display");
   });
   $(".cg-main_li_out").click(function(){
-      $(".cg-menu_tool").removeClass("display");
+      $(".cg-menu_tool").removeClass("cg-display");
   });
   // HYPERLINKS
   $('.cg-main_li').click(function() {
     $('.cg-main_li').removeClass('cg-main_li_active');
     $(this).addClass('cg-main_li_active');
   });
-  // BUTTON LOGIN
-  $(".cg-button_a > span").click(function(){
-    if($(".cg-button_a").hasClass("cg-log_on"))
-    {
-      $(".cg-button_a").removeClass("cg-log_on");
-      $(".cg-button_a").addClass("cg-log_off");
-      $(".cg-log_off figure, .cg-log_off span").empty();
-      $(".cg-log_off figure").append("<i class='fa fa-sign-in' aria-hidden='true'></i>");
-      $(".cg-log_off span").append("Ingresar");
-    }
-    else
-    {
-      $(".cg-button_a").removeClass("cg-log_off");
-      $(".cg-button_a").addClass("cg-log_on");
-      $(".cg-log_on figure, .cg-log_on span").empty();
-      $(".cg-log_on figure").append("<i class='fa fa-user' aria-hidden='true'></i>");
-      $(".cg-log_on span").append("<i class='fa fa-power-off' aria-hidden='true'></i>");
-    }
+  $('.cg-li_section_form').click(function() {
+    $('.cg-li_section_form').removeClass('cg-li_section_form_active');
+    $(this).addClass('cg-li_section_form_active');
   });
+  //POP UP
+    //OPEN INTERACTIONS
+      // REVEAL POP UP LOGIN
+      $(".cg-log_on").click(function(){
+          $(".cg-bg_popup").fadeIn("slow");
+          $(".cg-login").fadeIn("slow");
+          $(".cg-login").css({
+            "display":'flex'
+          });
+      });
+      //REVEAL POP UP EXCEPT LOGIN
+      $(".cg-pop_trigger").click(function(){
+          var id_pop;
+          id_pop = $(this).attr("id");
+          $(".cg-bg_popup").fadeIn("slow");
+          $(".cg-popup").fadeOut("fast");
+          $(".cg-" + id_pop).fadeIn("slow");
+          $(".cg-" + id_pop).css({
+            "display":'flex'
+          });
+      });
+    // CLOSE INTERACTIONS
+    $(".cg-close_pop").click(function(){
+      $(".cg-bg_popup, .cg-popup, .cg-wrapper_popup").css({"display":'none'});
+      $("#cg-popup, .cg-flex_pop").css({
+          'height':""
+        }).delay( 1000 );
+      $(".cg-wrapper_form_sections").animate({
+          "marginLeft":'0%'
+        }, 1000);
+      $('.cg-li_section_form').removeClass('cg-li_section_form_active');
+      $('.cg-li_section_form:first-of-type').addClass('cg-li_section_form_active');
+    });
 });
+// DISABLE AND ENABLE BODY SCROLL WHEN POPUP IS OPEN
+$(document).ready(function(){
+  var targetNodes         = $(".cg-popup");
+  var MutationObserver    = window.MutationObserver || window.WebKitMutationObserver;
+  var myObserver          = new MutationObserver (mutationHandler);
+  var obsConfig           = { attributes : true, attributeFilter : ['style'] };
+  // ADD A TARGET NODE TO THE OBESERVER. CAN ONLY ADD ONE NODE AT TIME
+  targetNodes.each ( function () {
+      myObserver.observe (this, obsConfig);
+  } );
+  function mutationHandler (mutationRecords) {
+    var activate_scroll = true;
+    $(".cg-popup").each(function( index ) {
+      if($(this).css("display") != "none"){
+        $('html, body').css({'overflow-y': 'hidden', 'height': '100%'});
+        activate_scroll = false;
+        return;
+      }
+    });
+    if(activate_scroll){
+      $('html, body').css({'overflow-y': 'auto', 'height': 'auto'});
+    }
+  }
+});
+
+//BUTTON LOGIN
+function log_in()
+{
+  $(".cg-log_on").fadeOut("slow", 
+    function()
+      {
+        $(".cg-log_off").fadeIn("slow");
+        $(".cg-bg_popup, .cg-popup, .cg-wrapper_popup").fadeOut("slow");
+      }
+    );
+}
+function log_off()
+{
+  $(".cg-log_off").fadeOut("slow", 
+    function()
+      {
+        $(".cg-log_on").fadeIn("slow");
+      }
+    );
+}
+
+function form_section_a()
+{
+  $(".cg-wrapper_form_sections").animate({
+    "marginLeft":'0%'
+  }, 1000);
+  $(".cg-register #cg-popup, .cg-register .cg-flex_pop").animate({
+    "height":'29.5rem'
+  }, 300);
+  $('.cg-li_section_form').removeClass('cg-li_section_form_active');
+  $('.cg-li_section_form:nth-of-type(1)').addClass('cg-li_section_form_active');
+}
+
+
+function form_section_b()
+{
+  $(".cg-wrapper_form_sections").animate({
+    "marginLeft":'-100%'
+  }, 1000);
+  $(".cg-register #cg-popup, .cg-register .cg-flex_pop").animate({
+    "height":'25rem'
+  }, 300);
+  $('.cg-li_section_form').removeClass('cg-li_section_form_active');
+  $('.cg-li_section_form:nth-of-type(2)').addClass('cg-li_section_form_active');
+}
+
+function form_section_c()
+{
+  $(".cg-wrapper_form_sections").animate({
+    "marginLeft":'-202%'
+  }, 1000);
+  $(".cg-register #cg-popup, .cg-register .cg-flex_pop").animate({
+    "height":'24rem'
+  }, 300);
+  $('.cg-li_section_form').removeClass('cg-li_section_form_active');
+  $('.cg-li_section_form:nth-of-type(3)').addClass('cg-li_section_form_active');
+}
+
 
 
