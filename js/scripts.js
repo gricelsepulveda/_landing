5ff762ee5cc83173207bbac032873c132718ddeb
Gricel Dayhanna Sepúveda Rozas <gr3c2l@gmail.com>
2017-02-22 03:39:52 -0300

    general progress

--- js/scripts.js
+++ js/scripts.js
@@ -164,5 +164,29 @@ function form_section_c()
   $('.cg-li_section_form:nth-of-type(3)').addClass('cg-li_section_form_active');
 }
 
-
+$(document).ready(function () {
+  var navpos = $('.cg-navbar_main').offset();
+  console.log(navpos.top);
+  $(window).bind('scroll', function () {
+      if ($(window).scrollTop() > navpos.top) {
+          $('.cg-navbar_main').addClass('cg-navbar_main_top');
+          $('.cg-navbar_main').removeClass('cg-navbar_main_bottom');
+          $('.cg-menu_tool').addClass('cg-menu_tool_top');
+          $('.cg-menu_tool').removeClass('cg-menu_tool_bottom');
+          $('.cg-nav_mobile').addClass('cg-nav_mobile_top');
+          $('.cg-nav_mobile').removeClass('cg-nav_mobile_bottom');
+      } else {
+          $('.cg-navbar_main').addClass('cg-navbar_main_bottom');
+          $('.cg-navbar_main').removeClass('cg-navbar_main_top');
+          $('.cg-menu_tool').addClass('cg-menu_tool_bottom');
+          $('.cg-nav_mobile').addClass('cg-nav_mobile_bottom');
+          $('.cg-nav_mobile').removeClass('cg-nav_mobile_top');
+      }
+  });
+  var divs = $('.cg-fade_effect');
+  $(window).on('scroll', function() {
+     var st = $(this).scrollTop();
+     divs.css({ 'opacity' : (1 - st/300) });
+  });
+});
 
